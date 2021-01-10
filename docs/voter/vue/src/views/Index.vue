842d39d7ea706dee199d13b2930bfbd23126f14d
fadeev <denis@fadeev.org>
2021-01-10 08:01:47 +0000

    voter

---
+++ docs/voter/vue/src/views/Index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div class="sp-container">
+      <sp-sign-in />
+      <sp-bank-balances />
+      <sp-token-send />
+    <poll-form />
+    <poll-list/>
+    </div>
+  </div>
+</template>
+
+<script>
+import * as sp from "@tendermint/vue";
+import PollForm from "../components/PollForm";
+import PollList from "../components/PollList";
+
+export default {
+  components: { ...sp, PollForm, PollList },
+};
+</script>
